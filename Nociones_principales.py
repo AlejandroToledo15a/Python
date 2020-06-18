deaca26350054806104dbfb614b438367c2424e6
Alejandro <alejandroduran@ciencias.unam.mx>
2020-06-18 18:06:52 -0500

    Commit cero

---
+++ Nociones_principales.py
@@ -0,0 +1,15 @@
+# Así ponemos comentarios
+
+# Nuestro primer Hola mundo
+print("Hola mundo")
+
+# Así declaramos variables
+a = 5
+b = 10
+
+# Realizamos operaciones de la siguiente forma
+suma = a+b
+print("La suma es:" + str(suma))
+
+
+
